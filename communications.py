a8519de5fb668ccdec434c8a4b7c5583916a0959
Camero15792 <45833861+Camero15792@users.noreply.github.com>
2019-04-06 10:29:48 -0600

    Update communications.py

--- communications.py
+++ communications.py
@@ -1,37 +1,46 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
 import calendar
 import datetime
 import time
+from email import encoders
 
-Gmail_Account = 'starksupply0@gmail.com'
-Password = 'business1579'
-# We may need to discontinue using Gmail as our server because they tend to block logins from hosting services
-# This file will grow to be a conveiniant way to send emails with attachments, texts and if need be recieve and download email attachements
+Gmail_Account = ''
+Password = ''
+BillPath = 'C:/Users/Cameron Stark/Documents/ViewBill.pdf'
 
-def send_reminder_email(name, email_address, rent, due_date):
-    name,rent = str(name), '${:,.2f}'.format(rent)
+
+
+def send_reminder_email(name, email_address, rent, due_date, utility_bill, number_tenants, util_month, util_portion):
+    if util_portion == 0:
+        utilities = 0
+    else:
+        utilities = utility_bill/number_tenants
+    total = rent + utilities
+    name,rent,utilities,total = str(name), '${:,.2f}'.format(rent),'${:,.2f}'.format(utilities),'${:,.2f}'.format(total)
     due_month = due_date.strftime('%B')
     due_day = due_date.strftime('%A')
 
     email_content = """
-                      <head>
-                        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-                      </head>
-                      <body>
-                        Hello {name},<br>&nbsp;&nbsp;&nbsp;&nbsp; This is a courtesy reminder that rent for {month} is due on {day},
-                         please pay the following amount to email address: {Gmail}<br><br>
-                         Rent: {Rent}<br><br>
-                          <u><a href="https://pay.google.com/payments/u/0/home#sendRequestMoney"> Click to Pay with Google </a></u>
-                      </body>
-                      """.format(name=name, month=due_month, day=due_day, Rent=rent, Gmail = Gmail_Account)
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      </head>
+      <body>
+        Hello {name},<br><br>&nbsp;&nbsp;&nbsp;&nbsp; This is a courtesy reminder that rent for {due_month} and Utilities
+         for {util_month} are due on {due_day},
+         please pay the following amounts to email address: coholdingsllc@gmail.com<br><br>
+         Rent: {rent} + Utilities: {utilities} = Total: {total}<br><br>
+          <u><a href="https://pay.google.com/payments/u/0/home#sendRequestMoney"> Click to Pay with Google </a></u>
+      </body>
+      """.format(name=name, due_month=due_month, due_day=due_day, rent=rent, util_month=util_month, utilities=utilities, total=total)
     TO = email_address
     FROM = Gmail_Account
-    py_mail("Courtesy Reminder", email_content, TO, FROM)
+    py_mail("Courtesy Reminder", email_content, TO, FROM, BillPath)
 
 
-def py_mail(SUBJECT, BODY, TO, FROM):
+def py_mail(SUBJECT, BODY, TO, FROM, ATTACHMENT='null'):
     """With this function we send out our html email"""
     # Create message container - the correct MIME type is multipart/alternative here!
     MESSAGE = MIMEMultipart('alternative')
@@ -47,11 +56,18 @@ def py_mail(SUBJECT, BODY, TO, FROM):
     # According to RFC 2046, the last part of a multipart message, in this case
     # the HTML message, is best and preferred.
     MESSAGE.attach(HTML_BODY)
-    # The actual sending of the e-mail
+    # Add attachment
+    if ATTACHMENT != 'null':
+        part = MIMEBase('application', "octet-stream")
+        with open(ATTACHMENT, 'rb') as file:
+            part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % 'ViewBill.pdf')
+        MESSAGE.attach(part)
     server = smtplib.SMTP('smtp.gmail.com:587')
     # Credentials (if needed) for sending the mail
-    password = Password
     server.starttls()
-    server.login(FROM, password)
+    server.login(FROM, Password)
     server.sendmail(FROM, [TO], MESSAGE.as_string())
     server.quit()
+
